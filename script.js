81c9724835dd4e1675e5faee98fe1fcd0fe66040
Arthur Queiroz <arthurqueiroz25@gmail.com>
2023-10-25 17:10:39 -0300

    update files

--- script.js
+++ script.js
@@ -1,10 +1,26 @@
 const form = document.getElementById('form')
+const imgAproved = '<img src="./images/aprovado.png" alt="Emoji celebrando">'
+const imgReproved = '<img src="./images/reprovado.png" alt="Emoji decepcioando">'
+
+
 
 form.addEventListener('submit', (e) => {
     e.preventDefault()
 
     const grade = document.getElementById('grade')
     const nameActivity = document.getElementById('name-activity')
+
+    let lines = '<tr>'
+    lines += `<td>${nameActivity.value}</td>`
+    lines += `<td>${grade.value}</td>`
+    lines+=`<td>${grade.value >= 7 ? imgAproved : imgReproved}</td>`
+    lines+=`</tr>`
+
+    const table = document.querySelector('tbody')
+    table.innerHTML += lines
+
+    nameActivity.value = ''
+    grade.value = ''
 
 
 })

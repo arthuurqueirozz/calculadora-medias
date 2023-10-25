b0f2b495486f56dc836e50922941805f339fb9ff
Arthur Queiroz <arthurqueiroz25@gmail.com>
2023-10-25 16:51:23 -0300

    add file script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+const form = document.getElementById('form')
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const grade = document.getElementById('grade')
+    const nameActivity = document.getElementById('name-activity')
+
+
+})
